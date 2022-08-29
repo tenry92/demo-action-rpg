bf516f0889c6ca52b7390280fde961049183b4ea
Simon "Tenry" Burchert <contact@simon-burchert.com>
2022-08-29 16:33:45 +0200

    Integrate basic animations and character models

--- unity/DemoActionRpg/Assets/Scripts/Runtime/Tenry/DemoActionRpg/PlayerController.cs
+++ unity/DemoActionRpg/Assets/Scripts/Runtime/Tenry/DemoActionRpg/PlayerController.cs
@@ -10,15 +10,31 @@ namespace Tenry.DemoActionRpg {
     [SerializeField]
     private float moveSpeed = 10f;
 
+    /// Model rotation in degrees per second.
+    [SerializeField]
+    private float rotationSpeed = 360f * 2f;
+
+    [SerializeField]
+    private Transform model;
+
     [SerializeField]
     private InputAction moveAction;
     #endregion
 
     private CharacterController characterController;
 
+    private Animator animator;
+
+    private float movementSpeed = 0f;
+
+    /// Direction in degrees.
+    private float facingDirection = 0f;
+
     private void Awake() {
       Debug.Assert(this.moveAction != null);
+      Debug.Assert(this.model != null);
       Debug.Assert(this.characterController = this.GetComponent<CharacterController>());
+      Debug.Assert(this.animator = this.GetComponentInChildren<Animator>());
     }
 
     private void OnEnable() {
@@ -35,12 +51,25 @@ namespace Tenry.DemoActionRpg {
 
     private void Update() {
       this.UpdateMovement();
+      this.UpdateAnimation();
     }
 
     private void UpdateMovement() {
       var input = this.moveAction.ReadValue<Vector2>();
 
-      this.characterController.Move(new Vector3(input.x, 0f, input.y) * this.moveSpeed * Time.deltaTime);
+      var moveVector = new Vector3(input.x, 0f, input.y) * this.moveSpeed;
+      this.characterController.Move(moveVector * Time.deltaTime);
+      this.movementSpeed = input.magnitude;
+
+      if (moveVector.magnitude > 0f) {
+        var targetRotation = Quaternion.LookRotation(moveVector, Vector3.up);
+        this.model.transform.rotation = Quaternion.RotateTowards(this.model.transform.rotation, targetRotation, this.rotationSpeed * Time.deltaTime);
+        this.facingDirection = Vector3.SignedAngle(Vector3.forward, this.model.transform.rotation * Vector3.forward, Vector3.up);
+      }
+    }
+
+    private void UpdateAnimation() {
+      this.animator?.SetFloat("MovementSpeed", this.movementSpeed);
     }
 
     private IEnumerable<InputAction> GetAllActions() {
